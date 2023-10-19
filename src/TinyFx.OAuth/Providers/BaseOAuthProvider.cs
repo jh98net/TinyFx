c9662a6760f1a490b25419468af9160db57c4318
jh98net <jh98net@sina.com>
2023-10-19 19:11:11 +0800

    1

--- src/TinyFx.OAuth/Providers/BaseOAuthProvider.cs
+++ src/TinyFx.OAuth/Providers/BaseOAuthProvider.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TinyFx.Caching;
+using TinyFx.Logging;
 using TinyFx.Net;
 
 namespace TinyFx.OAuth.Providers
@@ -55,7 +56,10 @@ namespace TinyFx.OAuth.Providers
         {
             var ret = new ResponseResult<OAuthUserDto>();
 
+            var log = LogUtil.GetContextLog();
+            log.AddField("oauth.ipo", SerializerUtil.SerializeJson(ipo));
             var value = await _dcache.GetStringAsync(ipo.State);
+            log.AddField("oauth.uuid", value);
             await _dcache.RemoveAsync(ipo.State);
             if (string.IsNullOrEmpty(value) || (!string.IsNullOrEmpty(ipo.Uuid) && value != "default" && ipo.Uuid != value))
             {
@@ -68,17 +72,23 @@ namespace TinyFx.OAuth.Providers
                 .AddUrl($"{UserInfoUrl}?access_token={ipo.AccessToken}")
                 .AppendUrl(AppendUserUrl())
                 .GetAsync<TSuccess, TError>();
+            log.AddField("oauth.rsp", SerializerUtil.SerializeJson(rsp));
             if (rsp.Success)
             {
                 ret.Success = true;
                 ret.Result = convertFunc(rsp.SuccessResult);
+                log.AddField("oauth.result", SerializerUtil.SerializeJson(ret.Result));
             }
             else
             {
                 ret.Success = false;
                 ret.Exception = rsp.Exception;
                 ret.Message = rsp.ResultString;
+                log.AddField("oauth.resultString", rsp.ResultString);
+                log.AddException(rsp.Exception);
             }
+            if (!log.IsContextLog)
+                log.SetFlag("OAUTH").Save();
             return ret;
         }
 
