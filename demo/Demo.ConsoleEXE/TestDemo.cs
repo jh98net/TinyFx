936ece61906633b6d17c5ae0a9b562dc586dcc2b
jh98net <jh98net@sina.com>
2023-10-24 04:16:33 +0800

    3.10.11

--- demo/Demo.ConsoleEXE/TestDemo.cs
+++ demo/Demo.ConsoleEXE/TestDemo.cs
@@ -51,21 +51,39 @@ using System.Linq.Expressions;
 using System.Reflection.Emit;
 using TinyFx.Extensions.IP2Country;
 using Google.Protobuf.WellKnownTypes;
+using TinyFx.Data.SqlSugar;
+using TinyFx.Reflection;
 
 namespace TinyFx.Demos
 {
+    public static class AAA
+    {
+        public static void Parse(Expression<Func<V_demo_user_courseEO>> expr)
+        {
+            var visitor = new MyExprVisitor();
+            visitor.Visit(expr);
+            var v = visitor.GetKeys();
+            Console.WriteLine(v.DictKey);
+            Console.WriteLine(v.ValueKey);
+        }
+    }
+
+
     internal class TestDemo : DemoBase
     {
         public override async Task Execute()
         {
-            Expression<Func<V_demo_user_courseEO>> expr = () => new V_demo_user_courseEO
+            var abc = "bbbb";
+            AAA.Parse(() => new V_demo_user_courseEO
             {
-                ClassID="a",
-                UserID=1
-            };
-            var visitor = new MyExprVisitor();
-            visitor.Visit(expr);
-            var a = visitor.GetKeys();
+                UserID = 1,
+                ClassID = "aaaaaa"
+            });
+            AAA.Parse(() => new V_demo_user_courseEO
+            {
+                UserID = 2,
+                ClassID = abc
+            });
         }
     }
     public class MyExprVisitor : ExpressionVisitor
@@ -81,8 +99,39 @@ namespace TinyFx.Demos
 
         protected override Expression VisitConstant(ConstantExpression node)
         {
-            ValueKeys.Add(Convert.ToString(node.Value));
+            if (ReflectionUtil.IsSimpleType(node.Type))
+                ValueKeys.Add(Convert.ToString(node.Value));
             return base.VisitConstant(node);
+        }
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            var value = GetMemberValue(node);
+            ValueKeys.Add(Convert.ToString(value));
+            return base.VisitMember(node);
+        }
+        private static object GetMemberValue(MemberExpression expression)
+        {
+            if (expression == null)
+                return null;
+            var field = expression.Member as FieldInfo;
+            if (field != null)
+            {
+                var constValue = GetConstantValue(expression.Expression);
+                return field.GetValue(constValue);
+            }
+            var property = expression.Member as PropertyInfo;
+            if (property == null)
+                return null;
+            var value = GetMemberValue(expression.Expression as MemberExpression);
+            return property.GetValue(value);
+        }
+
+        private static object GetConstantValue(Expression expression)
+        {
+            var constantExpression = expression as ConstantExpression;
+            if (constantExpression == null)
+                return null;
+            return constantExpression.Value;
         }
         public (string DictKey, string ValueKey) GetKeys()
         {
--- src/TinyFx.DbCaching/DbCacheMemoryExpressionVisitor.cs
+++ src/TinyFx.DbCaching/DbCacheMemoryExpressionVisitor.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using TinyFx.Reflection;
 
 namespace TinyFx.DbCaching
 {
@@ -20,8 +22,39 @@ namespace TinyFx.DbCaching
 
         protected override Expression VisitConstant(ConstantExpression node)
         {
-            ValueKeys.Add(Convert.ToString(node.Value));
+            if (ReflectionUtil.IsSimpleType(node.Type))
+                ValueKeys.Add(Convert.ToString(node.Value));
             return base.VisitConstant(node);
+        }
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            var value = GetMemberValue(node);
+            ValueKeys.Add(Convert.ToString(value));
+            return base.VisitMember(node);
+        }
+        private static object GetMemberValue(MemberExpression expression)
+        {
+            if (expression == null)
+                return null;
+            var field = expression.Member as FieldInfo;
+            if (field != null)
+            {
+                var constValue = GetConstantValue(expression.Expression);
+                return field.GetValue(constValue);
+            }
+            var property = expression.Member as PropertyInfo;
+            if (property == null)
+                return null;
+            var value = GetMemberValue(expression.Expression as MemberExpression);
+            return property.GetValue(value);
+        }
+
+        private static object GetConstantValue(Expression expression)
+        {
+            var constantExpression = expression as ConstantExpression;
+            if (constantExpression == null)
+                return null;
+            return constantExpression.Value;
         }
         public (string DictKey, string ValueKey) GetKeys()
         {
