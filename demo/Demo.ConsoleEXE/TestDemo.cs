daf5c7613f55930be89c6178e1524e7c012e2e09
蒋辉 <jh98net@sina.com>
2024-01-04 16:01:26 +0800

    CIDBug

--- demo/Demo.ConsoleEXE/TestDemo.cs
+++ demo/Demo.ConsoleEXE/TestDemo.cs
@@ -14,19 +14,6 @@ namespace TinyFx.Demos
     {
         public override async Task Execute()
         {
-            var client = RedisUtil.CreateStringClient<object>("TEST_SyncNotify");
-            var count = 0;
-            for (int i = 0; i < 1000000; i++)
-            {
-                var hashCode = Guid.NewGuid().ToString().GetHashCode();
-                var id = (long)hashCode + int.MaxValue + 1;
-                var result = await client.GetBitAsync(id);
-                if (result)
-                    count++;
-                await client.SetBitAsync(id, true);
-                Console.WriteLine($"{i}=>{count}");
-            }
-            Console.WriteLine(count);
         }
     }
 
